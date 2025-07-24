793dc934c777ad076b2166bbb1f4024d3e36ee2b
jayyyyte <138153456+jayyyyte@users.noreply.github.com>
2025-07-24 15:11:05 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace exercise2
+{
+    public struct Name
+    {
+        public string FirstName;
+        public string MiddleName;
+        public string LastName;
+
+        public override string ToString()
+        {
+            return $"{LastName} {MiddleName} {FirstName}";
+        }
+    }
+
+    public struct HomeTown
+    {
+        public string Commune;
+        public string District;
+        public string Province;
+
+        public override string ToString()
+        {
+            return $"{Commune}, {District}, {Province}";
+        }
+    }
+
+    public struct Score
+    {
+        public double Maths;
+        public double Physics;
+        public double Chemistry;
+
+        public double TotalScore()
+        {
+            return Maths + Physics + Chemistry;
+        }
+    }
+
+    public class THISINH
+    {
+        public Name Name;
+        public HomeTown HomeTown;
+        public string School = "";
+        public int Age;
+        public string IdNumber = "";
+        public Score Score;
+
+        public double TotalScore()
+        {
+            return Score.TotalScore();
+        }
+
+        public void EnterInfo()
+        {
+            Console.WriteLine("Enter candidate's information:");
+
+            Console.Write("Last name: ");
+            Name.LastName = Console.ReadLine() ?? "";       // operator ?? to assign default value as empty string ("") if input is null
+
+            Console.Write("Middle name: ");
+            Name.MiddleName = Console.ReadLine() ?? "";
+
+            Console.Write("First name: ");
+            Name.FirstName = Console.ReadLine() ?? "";
+
+            Console.Write("Commune: ");
+            HomeTown.Commune = Console.ReadLine() ?? "";
+
+            Console.Write("District: ");
+            HomeTown.District = Console.ReadLine() ?? "";
+
+            Console.Write("Province: ");
+            HomeTown.Province = Console.ReadLine() ?? "";
+
+            Console.Write("School: ");
+            School = Console.ReadLine() ?? "";
+
+            Console.Write("Age: ");
+            Age = int.Parse(Console.ReadLine() ?? "");
+
+            Console.Write("ID number: ");
+            IdNumber = Console.ReadLine() ?? "";
+
+            Console.Write("Maths score: ");
+            Score.Maths = double.Parse(Console.ReadLine() ?? "");
+
+            Console.Write("Physics score: ");
+            Score.Physics = double.Parse(Console.ReadLine() ?? "");
+
+            Console.Write("Chemistry score: ");
+            Score.Chemistry = double.Parse(Console.ReadLine() ?? "");
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"| {Name,-30} | {HomeTown,-35} | {IdNumber,-18} | {Score.Maths,-8} | {Score.Physics,-8} | {Score.Chemistry,-10} | {TotalScore(),-11} |");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            THISINH ts = new THISINH();
+            ts.EnterInfo();
+            Console.WriteLine();
+
+            TitleDisplay();
+            ts.DisplayInfo();
+            
+
+            Console.ReadKey();
+        }
+
+        static void TitleDisplay()
+        {
+            Console.WriteLine("---------------------------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine($"| {"Full Name",-30} | {"Hometown",-35} | {"ID Number",-18} | {"Maths",-8} | {"Physics",-8} | {"Chemistry",-10} | {"Total score",-8} |");
+            Console.WriteLine("---------------------------------------------------------------------------------------------------------------------------------------");
+        }
+    }
+}
